b2aea34a7a1468cbf6c9613096a78c6c15fb6c6e
MaximusDavid <maxifyhub01@gmail.com>
2026-03-05 11:55:54 +0100

    corrected settings in wsgi and asgi to point to folder

--- Maxify_Fitness/Maxify_Fitness/wsgi.py
+++ Maxify_Fitness/Maxify_Fitness/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Maxify_Fitness.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Maxify_Fitness.Maxify_Fitness.settings')
 
 application = get_wsgi_application()
